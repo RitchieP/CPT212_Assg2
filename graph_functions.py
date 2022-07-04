602226ec8e98fb81fc7b2814083422abc09d17ad
Ritchie <ritchiepoh@gmail.com>
2022-07-04 17:09:56 +0800

    Updated file with networkx library and plotted graph.

--- graph_functions.py
+++ graph_functions.py
@@ -1,32 +1,54 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# A global list of vertex to be used in the class
+# vertex_list = ["LA", "BL", "SB", "RM", "MV"]
+vertex_list = [
+    {"label": "LA", "fullname": "Los Angeles", "pos": (1, 4)},
+    {"label": "BL", "fullname": "Berlin", "pos": (5, 6)},
+    {"label": "SB", "fullname": "Salzburg", "pos": (5, 5)},
+    {"label": "RM", "fullname": "Rome", "pos": (5, 3)},
+    {"label": "MV", "fullname": "Montevideo", "pos": (2, 1)},
+]
+
 class Graph:
     # Constructor to initialize the matrix
-    def __init__(self, size):
-        self.adjacency_mat = []
-        for i in range(size):
-            self.adjacency_mat.append([0 for i in range(size)])
-        self.size = size
+    def __init__(self):
+        distance_LA_BL = 9310
+        distance_BL_SB = 526
+        distance_SB_RM = 659
+        distance_RM_MV = 11032
+        distance_MV_LA = 10020
 
-    # Add edges from vertex 1 to vertex 2, with a default weight of 1
-    def add_edges(self, v1, v2, weight=1):
-        if v1 == v2:
-            print(f"{v1} and {v2} are the vertices!")
-        self.adjacency_mat[v1][v2] = weight
+        self.graph = nx.DiGraph()
+        self.graph.add_nodes_from([
+            (vertex_list[0]["label"], {"pos": vertex_list[0]["pos"]}),
+            (vertex_list[1]["label"], {"pos": vertex_list[1]["pos"]}),
+            (vertex_list[2]["label"], {"pos": vertex_list[2]["pos"]}),
+            (vertex_list[3]["label"], {"pos": vertex_list[3]["pos"]}),
+            (vertex_list[4]["label"], {"pos": vertex_list[4]["pos"]}),
+        ])
 
-    # Remove edges
-    def remove_edges(self, v1, v2):
-        if self.adjacency_mat[v1][v2] == 0:
-            print(f"No edge between vertex {v1} and {v2}")
-        self.adjacency_mat[v1][v2] = 0
+        self.graph.add_weighted_edges_from([
+            (vertex_list[0]["label"], vertex_list[1]["label"], distance_LA_BL),
+            (vertex_list[1]["label"], vertex_list[2]["label"], distance_BL_SB),
+            (vertex_list[2]["label"], vertex_list[3]["label"], distance_SB_RM),
+            (vertex_list[3]["label"], vertex_list[4]["label"], distance_RM_MV),
+            (vertex_list[4]["label"], vertex_list[0]["label"], distance_MV_LA)
+        ])
 
-    # Print the matrix
-    def print_matrix(self):
-        vertex_list = [" ", "LA", "BL", "SB", "RM", "MV"]
-        for vertex in vertex_list:
-            print("{:4}".format(vertex), end=' ')
-        print()
+    def print_graph(self):
+        # subax1 = plt.subplot(121)
+        pos=nx.get_node_attributes(self.graph, "pos")
+        labels = nx.get_edge_attributes(self.graph, "weight")
+        nx.draw(self.graph, pos, with_labels=True, font_weight='bold')
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=labels)
+        plt.show()
+        # subax2 = plt.subplot(122)
+        # nx.draw(self.graph, nlist=[range])
 
-        for i, row in enumerate(self.adjacency_mat):
-            print(vertex_list[i+1], end=' ')
-            for val in row:
-                print("{:4}".format(val), end=' ')
-            print()
+
+
+    # Check if the graph has a cycle
+    #def check_cycle(self):
+
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import graph_functions
 
-distance_LA_BL = 9310
-distance_BL_SB = 526
-distance_SB_RM = 659
-distance_RM_MV = 11032
-distance_MV_LA = 10020
-
 if __name__ == "__main__":
-    graph = graph_functions.Graph(5)
-    graph.add_edges(0, 1, distance_LA_BL)
-    graph.add_edges(1, 2, distance_BL_SB)
-    graph.add_edges(2, 3, distance_SB_RM)
-    graph.add_edges(3, 4, distance_RM_MV)
-    graph.add_edges(4, 0, distance_MV_LA)
-
-    graph.print_matrix()
+    graph = graph_functions.Graph()
+    graph.print_graph()
