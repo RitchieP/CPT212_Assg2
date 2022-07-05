f9a4074c4a0164be5eeb346fc8b34f5eb046a08c
WeiRu00 <chinweiru@gmail.com>
2022-07-05 21:02:21 +0800

    added function 1

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import graph_functions
 
 
 if __name__ == "__main__":
+    
     graph = graph_functions.Graph()
-    graph.function_two()
+    graph.function_one()
+    # graph.function_two()
 
     # Printing the adjacency list for the graph
-    graph.print_adjacency_list()
+    # graph.print_adjacency_list()
