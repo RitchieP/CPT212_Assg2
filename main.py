2d15cd2f7a92826be2c36501831175815b5f0169
WeiRu00 <chinweiru@gmail.com>
2022-07-05 22:30:57 +0800

    Added user interface (incomplete)

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+import user_interface
 import graph_functions
-
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     
     graph = graph_functions.Graph()
-    graph.function_one()
-    # graph.function_two()
+    
+    # Homepage
+    user_interface.homepage()
+    plt.title("DEFAULT GRAPH")
+    graph.print_graph()
+    
+    start = True
+    while start:
+        # Prompt user to select a function
+        func = user_interface.user_input()
+
+        while func:
+            if func == 1:
+                graph.function_one()
+                break
+            elif func == 2:
+                graph.function_two()      
+                break
+            # other functions later laa when everything compile tgt
+            elif func == 5:
+                graph.add_new_edge()
+                break
+    
 
     # Printing the adjacency list for the graph
-    # graph.print_adjacency_list()
+    # graph.print_adjacency_list()1
+    
