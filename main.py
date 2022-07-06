5183b42ddc7d52b206d1771283db07df82a80fa1
WeiRu00 <chinweiru@gmail.com>
2022-07-06 12:36:53 +0800

    update main with simplified code

--- main.py
+++ main.py
@@ -6,28 +6,16 @@ if __name__ == "__main__":
     
     graph = graph_functions.Graph()
     
-    # Homepage
-    user_interface.homepage()
-    plt.title("DEFAULT GRAPH")
+    print("Default graph is as follow:")
     graph.print_graph()
-    
-    start = True
-    while start:
-        # Prompt user to select a function
-        func = user_interface.user_input()
 
-        while func:
-            if func == 1:
-                graph.function_one()
-                break
-            elif func == 2:
-                graph.function_two()      
-                break
-            # other functions later laa when everything compile tgt
-            elif func == 5:
-                graph.add_new_edge()
-                break
-    
+    while True:
+        user_interface.menu()
+        choice = user_interface.user_input()
+        if choice == 8:
+            break
+        user_interface.function_interface(choice, graph)
+
 
     # Printing the adjacency list for the graph
     # graph.print_adjacency_list()1
